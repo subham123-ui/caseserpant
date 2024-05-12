2b4dd9abd70f5e92d3e96e4bbca49294a5996ace
subham123-ui <suvam.rockstar@gmail.com>
2024-05-12 23:52:39 +0530

    strt

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative  mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div></div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
