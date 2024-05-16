5f1b6df9bf697f7c811373d6eaf72b3cac655580
subham123-ui <suvam.rockstar@gmail.com>
2024-05-16 23:00:25 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
 
 const Navbar = () => {
+  const user = undefined;
+
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -9,6 +12,32 @@ const Navbar = () => {
           <Link href="/" className="flex z-40 font-semibold">
             case<span className="text-green-600">serpant</span>
           </Link>
+          <div className="h-full flex items-center space-x-4 ">
+            {user ? (
+              <>
+                <link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign out
+                </link>
+                {<link
+                  href="/api/auth/logout"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign out
+                </link>}
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
       </MaxWidthWrapper>
     </nav>
