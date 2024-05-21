f6e0c2211b893a506d4fe60bb231e2aed2181b14
subham123-ui <suvam.rockstar@gmail.com>
2024-05-21 23:34:56 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,6 +150,35 @@ export default function Home() {
                 </div>
               </div>
             </div>
+            {/* user 2 review */}
+            <div className="flex flex-auto flex-col  gap-4 lg:pr-8 xl:pr-20 ">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                <Star className="h-5 w-5 text-green-500 fill-green-600" />
+                {/* <Star className="h-5 w-5 text-green-500 fill-green-600" /> */}
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I usually keep my phone together with my keys in my pocket
+                  and that led to some pretty heavy scratchmarks on all of my
+                  last phone cases. This one, besides a barely noticeable
+                  scratch on the corner,{" "}
+                  <span className="p-0.5 bg-slate-800 text-white">looks brand new after about half a year</span>. I dig it."
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img src="/users/user-4.jpg" alt="user" className="rounded-full h-12 w-12 object-cover" />
+                <div className="flex flex-col ">
+                  <p className="font-semibold ">Javier</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
