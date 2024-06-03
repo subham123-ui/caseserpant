284b5acd69631615bbd23bd5ed8a43e9233da304
subham123-ui <suvam.rockstar@gmail.com>
2024-06-03 23:55:38 +0530

    frntend

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -2,14 +2,34 @@
 /* eslint-disable @next/next/no-img-element */
 import { useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { useInView } from "framer-motion";
+
+const PHONES = [
+  "/testimonials/1.jpg",
+  "/testimonials/2.jpg",
+  "/testimonials/3.jpg",
+  "/testimonials/4.jpg",
+  "/testimonials/5.jpg",
+  "/testimonials/6.jpg",
+];
+
+function splitArray<T>(array: Array<T>, numParts: number) {
+  const result: Array<Array<T>> = [];
+}
 
 function ReviewGrid() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
-  const containerRef = useRef<HTMLDivElement | null>(null)
+  const isInView = useInView(containerRef, { once: true, amount: 0.4 });
 
-  return <div ref={containerRef} className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"></div>
+  const colums = splitArray(PHONES, 3);
 
-
+  return (
+    <div
+      ref={containerRef}
+      className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"
+    ></div>
+  );
 }
 
 export function Reviews() {
