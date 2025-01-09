e162b40ef756e61f13a265cceb17807f3def68ab
subham123-ui <suvam.rockstar@gmail.com>
2025-01-09 23:44:40 +0530

    fr

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -3,6 +3,7 @@ import { db } from '@/db'
 import { notFound } from 'next/navigation'
 import DesignConfigurator from './DesignConfigurator'
 
+
 interface PageProps {
   searchParams: {
     [key: string]: string | string[] | undefined
